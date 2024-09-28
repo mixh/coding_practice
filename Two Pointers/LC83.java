63d4b86c2182b3389d73f023122e47898798ab28
Mihir Jain <63667062+mixh@users.noreply.github.com>
2024-09-27 23:37:33 -0400

    list nodes

---
+++ Two Pointers/LC83.java
@@ -0,0 +1,17 @@
+// Remove Duplicates from Sorted List
+public class LC83 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode deleteDuplicates(ListNode head) {
+        
+
+        
+
+        return head;
+    }
+}
