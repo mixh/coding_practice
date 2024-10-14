a22631607dba39629cabd3db38ecca8e7b30a5c5
Mihir Jain <63667062+mixh@users.noreply.github.com>
2024-10-13 20:10:53 -0400

    LC2530 q oct 13

---
+++ October LC Challenge/LC2530.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class LC2530 {
+
+    public long maxKelements(int[] nums, int k) {
+        long ans = 0;
+        // Create a max-heap to store the elements
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            Collections.reverseOrder()
+        );
+
+        for (int i : nums) {
+            pq.add(i);
+        }
+
+        while (k-- > 0) {
+            // Add the maxElement in ans and push its one-third value in the
+            // priority queue
+            int maxElement = pq.poll();
+            ans += maxElement;
+            pq.add((int) Math.ceil(maxElement / 3.0));
+        }
+        return ans;
+    }
+}
