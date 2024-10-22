a4d504c6faa88b7937284ee9d1e3ee7b87652979
Mihir Jain <63667062+mixh@users.noreply.github.com>
2024-10-21 21:12:46 -0400

    lc2583 tree med oct 21

---
+++ October LC Challenge/LC2583.java
@@ -0,0 +1,52 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+class LC2583 {
+
+   public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        // max heap
+        PriorityQueue<Long> pq = new PriorityQueue<>(
+            Collections.reverseOrder()
+        );
+
+        Queue<TreeNode> bfsQueue = new LinkedList<>();
+        bfsQueue.add(root);
+        while (!bfsQueue.isEmpty()) {
+            //level order traversal
+            int size = bfsQueue.size();
+            long sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode poppedNode = bfsQueue.remove();
+                sum += poppedNode.val;
+                if (poppedNode.left != null) {
+                    //add left child
+                    bfsQueue.add(poppedNode.left);
+                }
+                if (poppedNode.right != null) {
+                    // add right child
+                    bfsQueue.add(poppedNode.right);
+                }
+            }
+            pq.add(sum);
+        }
+        if (pq.size() < k) return -1;
+        for (int i = 0; i < k - 1; i++) pq.remove();
+        return pq.peek();
+    }
+}
