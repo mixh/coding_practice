82b41af49fcb438954f6a47cda2ed6e0208616ae
Mihir Jain <63667062+mixh@users.noreply.github.com>
2024-10-22 21:23:31 -0400

    binary tree cousins oct 22 med

---
+++ October LC Challenge/LC2641.java
@@ -0,0 +1,55 @@
+class Solution {
+
+public class TreeNode {
+int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    private int[] levelSums = new int[100000];
+
+    public TreeNode replaceValueInTree(TreeNode root) {
+        calculateLevelSum(root, 0);
+        replaceValueInTreeInternal(root, 0, 0);
+        return root;
+    }
+
+    private void calculateLevelSum(TreeNode node, int level) {
+        if (node == null) {
+            return;
+        }
+
+        levelSums[level] += node.val;
+        calculateLevelSum(node.left, level + 1);
+        calculateLevelSum(node.right, level + 1);
+    }
+
+    private void replaceValueInTreeInternal(
+        TreeNode node,
+        int siblingSum,
+        int level
+    ) {
+        if (node == null) {
+            return;
+        }
+
+        int leftChildVal = (node.left == null) ? 0 : node.left.val;
+        int rightChildVal = (node.right == null) ? 0 : node.right.val;
+
+        if (level == 0 || level == 1) {
+            node.val = 0;
+        } else {
+            node.val = levelSums[level] - node.val - siblingSum;
+        }
+
+        replaceValueInTreeInternal(node.left, rightChildVal, level + 1);
+        replaceValueInTreeInternal(node.right, leftChildVal, level + 1);
+    }
+}
